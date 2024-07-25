95a2c073236e8dc60dc2c5c2c8b11828f4e2428c
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-25 09:22:40 +0100

    Feat: Album model created.

---
+++ app/src/main/java/com/example/recordshopv2frontend/model/Album.java
@@ -0,0 +1,68 @@
+package com.example.recordshopv2frontend.model;
+
+public class Album {
+    private long id;
+    private String albumName;
+    private Artist artist;
+    private String genre;
+    private String artUrl;
+    private int releaseYear;
+    private int stockQuantity;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtUrl() {
+        return artUrl;
+    }
+
+    public void setArtUrl(String artUrl) {
+        this.artUrl = artUrl;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+    
+}
--- app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.recordshopv2frontend.ui;
+package com.example.recordshopv2frontend.ui.mainactivity;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
