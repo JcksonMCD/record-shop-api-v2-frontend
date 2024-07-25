a15a4a066877741035a396789e595e890e0c3666
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-25 09:51:56 +0100

    Feat: Retrofit and GSON dependencies added. Retrofit instance class body also now coded in.

--- app/src/main/java/com/example/recordshopv2frontend/service/RetrofitInstance.java
+++ app/src/main/java/com/example/recordshopv2frontend/service/RetrofitInstance.java
@@ -1,4 +1,21 @@
 package com.example.recordshopv2frontend.service;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+
+    private static final String baseURL = "http://10.0.2.2:8080/api/v2/";
+
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
