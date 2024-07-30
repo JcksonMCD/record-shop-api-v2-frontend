54945509a1f8a03ede2a344a02f4b6c7eb1b5492
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-30 16:34:52 +0100

    Feat: appropriate fields added to add album click handler class.

--- app/src/main/java/com/example/recordshopv2frontend/ui/addalbum/AddAlbumClickHandler.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/addalbum/AddAlbumClickHandler.java
@@ -1,4 +1,12 @@
 package com.example.recordshopv2frontend.ui.addalbum;
 
+import android.content.Context;
+
+import com.example.recordshopv2frontend.model.Album;
+import com.example.recordshopv2frontend.ui.mainactivity.MainActivityViewModel;
+
 public class AddAlbumClickHandler {
+    Album album;
+    Context context;
+    MainActivityViewModel mainActivityViewModel;
 }
