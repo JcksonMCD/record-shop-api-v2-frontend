0b2915651fce821f6bdf72ac82f886bfffd1e7cc
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-30 16:30:18 +0100

    Feat: Album repository field added to view model class.

--- app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityViewModel.java
@@ -1,4 +1,7 @@
 package com.example.recordshopv2frontend.ui.mainactivity;
 
+import com.example.recordshopv2frontend.model.AlbumRepository;
+
 public class MainActivityViewModel {
+    AlbumRepository albumRepository;
 }
