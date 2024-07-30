fe0c29f4f7ad05f5214cc79ab2635b38fdda9476
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-30 16:54:53 +0100

    Feat: Add album click handler method added to main click handler class.

--- app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityClickHandler.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityClickHandler.java
@@ -1,11 +1,20 @@
 package com.example.recordshopv2frontend.ui.mainactivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import com.example.recordshopv2frontend.ui.addalbum.AddAlbum;
 
 public class MainActivityClickHandler {
     Context context;
 
     public MainActivityClickHandler(Context context) {
         this.context = context;
+    }
+
+    public void addAlbumButtonClicked(View view) {
+        Intent intent = new Intent(view.getContext(), AddAlbum.class);
+        context.startActivity(intent);
     }
 }
