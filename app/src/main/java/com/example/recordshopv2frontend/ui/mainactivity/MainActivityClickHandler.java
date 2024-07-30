63c622c92df9513fabcf107f49899286531c17ec
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-30 16:54:00 +0100

    Feat: Context field and all args constructor added to main activity click handler class.

--- app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityClickHandler.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityClickHandler.java
@@ -1,4 +1,11 @@
 package com.example.recordshopv2frontend.ui.mainactivity;
 
+import android.content.Context;
+
 public class MainActivityClickHandler {
+    Context context;
+
+    public MainActivityClickHandler(Context context) {
+        this.context = context;
+    }
 }
