454464e0d21d8d491947789bb1034fe8561dbaa1
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-30 21:16:30 +0100

    Fix: View Model class changed to handle repo needing a context field now.

--- app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivityViewModel.java
@@ -1,19 +1,21 @@
 package com.example.recordshopv2frontend.ui.mainactivity;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 
 import com.example.recordshopv2frontend.model.Album;
 import com.example.recordshopv2frontend.model.AlbumRepository;
 
-public class MainActivityViewModel extends ViewModel {
-    AlbumRepository albumRepository = new AlbumRepository();
+public class MainActivityViewModel extends AndroidViewModel {
+    private final AlbumRepository albumRepository;
 
-    public MainActivityViewModel() {
-
+    public MainActivityViewModel(Application application) {
+        super(application);
+        this.albumRepository = new AlbumRepository(application);
     }
 
     public void addAlbum(Album album) {
         albumRepository.addAlbum(album);
     }
-
 }
