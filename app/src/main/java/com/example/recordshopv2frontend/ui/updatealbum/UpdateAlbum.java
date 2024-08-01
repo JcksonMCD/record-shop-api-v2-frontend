71a27dd5b7a12dc83821c8a368b2ff16b55e3b98
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-01 14:26:10 +0100

    Feat: Update album activty class on create method fully implemented.

--- app/src/main/java/com/example/recordshopv2frontend/ui/updatealbum/UpdateAlbum.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/updatealbum/UpdateAlbum.java
@@ -1,5 +1,6 @@
 package com.example.recordshopv2frontend.ui.updatealbum;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,25 +8,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.recordshopv2frontend.R;
 import com.example.recordshopv2frontend.databinding.ActivityUpdateAlbumBinding;
 import com.example.recordshopv2frontend.model.Album;
+import com.example.recordshopv2frontend.ui.mainactivity.MainActivityViewModel;
 
 public class UpdateAlbum extends AppCompatActivity {
     ActivityUpdateAlbumBinding activityUpdateAlbumBinding;
     UpdateAlbumClickHandler updateAlbumClickHandler;
     Album album;
 
+    private static final String ALBUM_KEY = "album";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_update_album);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            album = getIntent().getParcelableExtra("ALBUM_KEY", Album.class);
+        }
+
+        activityUpdateAlbumBinding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_update_album
+        );
+
+        activityUpdateAlbumBinding.setAlbum(album);
+
+        MainActivityViewModel model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        updateAlbumClickHandler = new UpdateAlbumClickHandler(
+                this,
+                album,
+                model
+        );
+
+        activityUpdateAlbumBinding.setClickHandler(updateAlbumClickHandler);
     }
 }
