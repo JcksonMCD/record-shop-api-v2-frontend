db54305bd2fddba24d66f81550a500c90d41d03d
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-01 14:27:22 +0100

    Feat: All appropriate fields and all args constructor added to update album click handler class.

--- app/src/main/java/com/example/recordshopv2frontend/ui/updatealbum/UpdateAlbumClickHandler.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/updatealbum/UpdateAlbumClickHandler.java
@@ -1,4 +1,19 @@
 package com.example.recordshopv2frontend.ui.updatealbum;
 
+import android.content.Context;
+
+import com.example.recordshopv2frontend.model.Album;
+import com.example.recordshopv2frontend.ui.mainactivity.MainActivityViewModel;
+
 public class UpdateAlbumClickHandler {
+    private Context context;
+    private Album album;
+    private MainActivityViewModel model;
+    private long albumID;
+
+    public UpdateAlbumClickHandler(Context context, Album album, MainActivityViewModel model) {
+        this.context = context;
+        this.album = album;
+        this.model = model;
+    }
 }
