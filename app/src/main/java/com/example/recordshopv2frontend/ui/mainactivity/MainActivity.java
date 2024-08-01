5d3613fb25598d2bcec325625f987565a0059e94
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-01 16:26:04 +0100

    Feat: Implementing search view in main activity added to on create.

--- app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/recordshopv2frontend/ui/mainactivity/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.recordshopv2frontend.ui.mainactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.SearchView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -26,29 +30,50 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements RecyclerViewInterface {
+public class MainActivity extends AppCompatActivity implements RecyclerViewInterface, AdapterView.OnItemSelectedListener {
 
     private RecyclerView recyclerView;
     private AlbumAdapter albumAdapter;
     private List<Album> albumList = new ArrayList<>();
     private ActivityMainBinding binding;
-    SearchView searchView;
-    Spinner spinner;
-    String spinnerSelection;
+    private SearchView searchView;
+    private Spinner spinner;
+    private String spinnerSelection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setClickHandler(new MainActivityClickHandler(this));
 
-        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = binding.recyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         albumAdapter = new AlbumAdapter(albumList, this);
         recyclerView.setAdapter(albumAdapter);
+
+        spinner = binding.dropdown;
+        spinner.setOnItemSelectedListener(this);
+        String[] dropItems = getResources().getStringArray(R.array.);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dropItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        searchView = binding.searchBar;
+        searchView.clearFocus();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
 
         fetchAlbums();
     }
@@ -61,10 +86,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
             @Override
             public void onResponse(Call<AlbumResponse> call, Response<AlbumResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    AlbumResponse albumResponse = response.body();
-                    albumList.clear(); // Clear existing data
-                    albumList.addAll(albumResponse.getContent()); // Add new data
-                    albumAdapter.notifyDataSetChanged(); // Notify adapter of data change
+                    albumList.clear();
+                    albumList.addAll(response.body().getContent());
+                    albumAdapter.notifyDataSetChanged();
                 } else {
                     Log.e("MainActivity", "Failed to fetch albums: " + response.message());
                 }
@@ -73,6 +97,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
             @Override
             public void onFailure(Call<AlbumResponse> call, Throwable t) {
                 Log.e("MainActivity", "Error fetching albums", t);
+                Toast.makeText(MainActivity.this, "Error fetching albums", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -83,4 +108,5 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         intent.putExtra("ALBUM_KEY", albumList.get(position));
         startActivity(intent);
     }
+
 }
